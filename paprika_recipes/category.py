a4b592abfda804d8febdf6fd0bf787f55c8a6eb3
Daniel Wehner <dawehner@googlemail.com>
2020-12-30 12:36:27 +0000

    Add category data class

---
+++ paprika_recipes/category.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+
+from dataclasses import dataclass, field, fields, asdict
+
+@dataclass
+class Category:
+    name : str
+    uid : str
+    parent_uid: str = ""
+    order_flag: int = 0
+
+    def as_json(self):
+        return json.dumps(self.as_dict())
+
+    def as_dict(self):
+        return asdict(self)
+    
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"<{self}>"
